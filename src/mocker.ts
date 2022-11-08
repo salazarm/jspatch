734aabc4f483c27d3f67e7eeca4c4286e0b79c54
Marco Salazar <marco@elementl.com>
2022-11-07 22:22:27 -0500

    use test types

--- src/mocker.ts
+++ src/mocker.ts
@@ -1,8 +1,3 @@
-/**
- * MockerImplementation, we will use toString to get the raw text and insert this at spot where we want to mock
- *
- * Generic mock
- */
 function MockerImplementation() {}
 
 const identifers = new Map();
--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -4,49 +4,47 @@ import { useCar, useOtherCar } from "./car";
 const NOT_PATCHED = "Not patched!";
 
 describe("Mock", () => {
-  it("can patch a value at the global scope", () => {
-    const result1 = useCar();
-    expect(result1).toBe(NOT_PATCHED);
+  TEST_TYPES.forEach((obj) => {
+    it("can patch a value at the global scope", () => {
+      const result1 = useCar();
+      expect(result1).toBe(NOT_PATCHED);
 
-    const unpatch = patch("src.test.car.useHook", () => () => ({
-      Hello: "World",
-    }));
+      const unpatch = patch("src.test.car.useHook", () => () => obj);
 
-    const result2 = useCar();
-    expect(result2).toBe({
-      randomHook: { Hello: "World" },
-      value1: NOT_PATCHED,
-      value2: NOT_PATCHED,
+      const result2 = useCar();
+      expect(result2).toBe({
+        randomHook: obj,
+        value1: NOT_PATCHED,
+        value2: NOT_PATCHED,
+      });
+
+      unpatch();
+
+      const result3 = useCar();
+      expect(result3).toBe({
+        randomHook: NOT_PATCHED,
+        value1: NOT_PATCHED,
+        value2: NOT_PATCHED,
+      });
     });
 
-    unpatch();
+    it("can patch a value at the local scope", () => {
+      // useHook` is patched for useCar but useOtherCar is unaffected
+      const unpatch = patch("src.test.car.useCar.useHook", () => () => obj);
 
-    const result3 = useCar();
-    expect(result3).toBe({
-      randomHook: NOT_PATCHED,
-      value1: NOT_PATCHED,
-      value2: NOT_PATCHED,
-    });
-  });
+      const car = useCar();
+      const otherCar = useOtherCar();
+      expect(car).toBe({
+        randomHook: obj,
+        value1: NOT_PATCHED,
+        value2: NOT_PATCHED,
+      });
 
-  it("can patch a value at the local scope", () => {
-    // useHook` is patched for useCar but useOtherCar is unaffected
-    const unpatch = patch("src.test.car.useCar.useHook", () => () => ({
-      Hello: "World",
-    }));
-
-    const car = useCar();
-    const otherCar = useOtherCar();
-    expect(car).toBe({
-      randomHook: { Hello: "World" },
-      value1: NOT_PATCHED,
-      value2: NOT_PATCHED,
-    });
-
-    expect(otherCar).toBe({
-      randomHook: NOT_PATCHED,
-      value1: NOT_PATCHED,
-      value2: NOT_PATCHED,
+      expect(otherCar).toBe({
+        randomHook: NOT_PATCHED,
+        value1: NOT_PATCHED,
+        value2: NOT_PATCHED,
+      });
     });
   });
 });
