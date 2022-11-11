0065e3a0574106adfed4081b3a9f91ff56716d3b
Marco polo <salazarm@users.noreply.github.com>
2022-11-11 17:05:19 -0500

    Update patchTemplate.js

--- src/patchTemplate.js
+++ src/patchTemplate.js
@@ -1,10 +1,6 @@
 /**
  * This is a stub that will be converted into an AST to be inserted at
  * locations that we want to be able to dynamically patch.
- *
- * Then at all callsites referencing this binding we will replace the
- *  node with a call to get the real value
- *
  */
 export function __PATCH_TEMPLATE__() {
   // Pass a getter so that we can use the original implementation lazily only if its needed.
